1c9cab2ab98c1378882830d6fac12fa71fe1748f
Samoht07 <341737@talnet.nl>
2019-12-06 11:53:56 +0100

    Uitwerking van de exercise

---
+++ registratie_handler.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Handler</title>
+</head>
+<body>
+<h1>Forum registratie</h1>
+<?php
+echo "Jouw username is " . $_POST['username'];
+echo "<br>";
+echo "Jouw email is " . $_POST['email'];
+echo "<br>";
+echo "Jouw leeftijd is " . $_POST['age'];
+?>
+</body>
+</html>
